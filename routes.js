6d06bca205b247894042fb88dd43b661dbcffa7d
David Deaton <dideater@gmail.com>
2020-08-30 14:54:09 -0700

    updated routes

---
+++ routes.js
@@ -0,0 +1,58 @@
+// Establishing a route to the db.json file required for the note data, making an array to store respones to write to the db
+const fs = require("fs");
+const path = require("path");
+const notesData = require(path.join(__dirname, '../db/db.json'));
+
+// Handling what to do when receiving API requests
+module.exports = function(app) {
+  // API GET Requests, returning a json file from the db.json file
+  app.get("/api/notes", (req, res) => {
+    res.json(notesData);
+  });
+
+  // API POST Requests
+  app.post("/api/notes", (req, res) => {
+    // Define where the object containing the note is in the request
+      const note = req.body;
+      // Assigning the note object a random id from 1-1000 and pushing it to the db.json, which is an array of objects
+      // ***Figure out how to ensure no new note receives an existing id # in future versions***, maybe use a for loop to go through the db.json, and if note.id === an existing notesData[i].id, run the randomizer again until it hits a free one, and if there are 1000 notes already, return an indication to the user to delete some notes. Could increase the random number range to make it get stuck doing this minimally, and cap the user at an amount of notes far lower than the randomizer, perhaps by if notesData.length > ~100 it returns an error and prompts to delete notes.
+
+      note.id = Math.ceil(Math.random() * 1000);
+      notesData.push(note);
+      // return the current note
+      res.json(note);
+
+      // Re-writing the db.json file with the newly pushed object
+      fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify(notesData), null, 2)
+
+    }
+  );
+  // Create an app.delete, each note should have a unique url and this function will loop through the information from db.json to find the corresponding note for that url/id, remove it, then rewrite the db.json file
+  app.delete("/api/notes/:id", (req, res) => {
+    // Defining the part of the request that we will use as the id to identify the array index in the db.json
+    let searchID = parseInt(req.params.id);
+
+    // A function that loops through db.json, finds the index that contains an ID matching the request, splices it out, and rewrites the db.json without that index
+
+    function findID() {
+      for (let i = 0; i < notesData.length; i++) {
+        if (notesData[i].id === searchID) {
+          notesData.splice(i, 1);
+
+          // Re-writing the db.json file with the newly spliced db.json
+           fs.writeFileSync(
+            path.join(__dirname, '../db/db.json'),
+            JSON.stringify(notesData), null, 2)
+            // Sending the newly spliced and written db.json to the client
+            res.json(notesData);
+
+            
+        }
+      }  
+    }
+    findID();
+  });
+
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+
+// Tells node that we are creating an "express" server
+const app = express();
+
+// Sets an initial port
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Ensuring static files, like the js and css, can be used
+app.use(express.static('public'));
+
+//Setting up the location of the routes to be used
+require("routes")(app);
+require("htmlroutes")(app);
+
+
+// Starting the server
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
